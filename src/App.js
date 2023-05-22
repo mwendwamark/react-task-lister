e4707c022be0285ed5403157e79e4ce634fa8db4
mark mwendwa <mwendwamark310@gmail.com>
2023-05-22 14:28:31 +0300

    starting off the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Form from "./components/Form";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import About from "./components/About";
+// import "./styling/App.css";
+import Contacts from "./components/Contacts";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <h2>hello world</h2>
+      <Routes>
+
+        <Route path="/login" element={<Login />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/your-todo-list" element={<Form />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
